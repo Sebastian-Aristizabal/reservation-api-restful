bdb31df4eefee3f952755333a7ad74b4def41f1c
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-05-18 21:46:40 -0500

    user validations, recureradom requirement and has secure random

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  require 'securerandom'
+  has_secure_password
+  has_many :reservations
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validate :username, presence: true, uniqueness: true
 end

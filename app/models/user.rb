5ecd7c6fef766b8357127c2086f39f7737a296da
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-05-18 23:06:08 -0500

    validation added directly in the database for users and some validations changes in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :reservations
 
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :password, presence: true, length: { minimum: 6 }
-  validate :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
--- db/migrate/20230519021625_create_users.rb
+++ db/migrate/20230519021625_create_users.rb
@@ -3,8 +3,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.string :username
-      t.string :email
-      t.string :password_digest
+      t.string :email, null: false
+      t.index :email, unique: true
+      t.string :password_digest, null: false
 
       t.timestamps
     end

373b4d19e5af396807b9829596769d288d021443
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-05-22 01:07:57 -0500

    Setup tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :tokens, only: [:create]
       resources :users, only: %i[show create update destroy]
       resources :restaurants do
         resources :tables
